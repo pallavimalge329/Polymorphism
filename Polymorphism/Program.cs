b8c720d8f75d5fe21b4094fa17db0d8d369c8175
HP <HP@DESKTOP-N5QBMVO>
2021-09-03 23:34:21 +0530

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Greater
+    {
+        public void max()
+        {
+            Console.WriteLine("without parameters");
+        }
+        public void max(int a, int b)
+        {
+
+        }
+
+        public float max(float a, float b)
+        {
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Greater g = new Greater();
+            g.max(23, 34);
+            g.max(10.3F, 20.3F);
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
